e02fa5197170d234e33ff92e216391816a24adc4
Steven <digitalexile99@gmail.com>
2023-10-25 17:13:45 -0600

    final es lint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import ActiveSectionContextProvider from "./context/active-section-context";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Steven | Personal Portfolio",
+  description: "Steven is a full-stack developer with 8 years of experience.",
 };
 
 export default function RootLayout({
